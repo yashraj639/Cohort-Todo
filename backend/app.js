db86798ac711c98b088f09b68f59455666f0ef9a
Yashraj <yashrajyadav4660@gmail.com>
2025-10-01 12:28:30 +0530

    backend: cors fix

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: "*", // auto-handle all
 };
 
 app.use(cors(corsOptions));
