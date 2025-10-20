cdb9a40adafeb042e5977caa2244a516ce5078a1
Yashraj <yashrajyadav4660@gmail.com>
2025-10-20 19:02:59 +0530

    fix: issue

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,6 @@ const todoRoutes = require("./routes/todo.routes");
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
 app.use("/api/auth", authRoutes);
 app.use("/api/todo", todoRoutes);
 
